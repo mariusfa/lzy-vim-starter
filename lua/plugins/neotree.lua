95a2a27045b5e97901912436e7bded516d144da0
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-04-02 08:43:22 +0200

    Add update

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "float",
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node",
+      },
+    },
+    filesystem = {
+      filtered_items = { hide_dotfiles = false, hide_gitignore = false },
+    },
+  },
+}
