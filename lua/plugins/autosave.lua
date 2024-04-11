39feae8a77d2de1d5d45f116fa61a88df6ce1113
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-04-11 09:22:39 +0200

    update

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,3 +1,9 @@
 return {
-  "pocco81/auto-save.nvim",
+  "okuuva/auto-save.nvim",
+  cmd = "ASToggle", -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  opts = {
+    -- your config goes here
+    -- or just leave it empty :)
+  },
 }
