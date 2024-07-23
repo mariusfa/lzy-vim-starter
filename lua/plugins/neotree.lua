f5589c5ba292b071c7dcba8e8edde4c2733c55c1
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-07-23 12:59:38 +0200

    Updaste nvim config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    popup_border_style = "rounded",
     window = {
       position = "float",
-      mappings = {
-        ["l"] = "open",
-        ["h"] = "close_node",
-      },
     },
     filesystem = {
       filtered_items = { hide_dotfiles = false, hide_gitignore = false },
@@ -14,11 +11,12 @@ return {
   },
   keys = {
     {
-      "<leader>fe",
+      "<leader>fE",
       function()
-        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root(), reveal = true })
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
       end,
-      desc = "Explorer NeoTree (Root Dir)",
+      desc = "Explorer NeoTree (cwd)",
     },
+    { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
   },
 }
