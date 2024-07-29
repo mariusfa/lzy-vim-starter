2b36d812449dc48dd49b9d96f87fc3529527f67c
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-07-29 09:26:01 +0200

    wip

---
+++ lua/plugins/noneckpain.lua
@@ -0,0 +1,10 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",
+  opts = {
+    width = 150,
+  },
+  keys = {
+    { "<leader>nn", ":NoNeckPain<CR>", { desc = "No neck pain, center layout" } },
+  },
+}
