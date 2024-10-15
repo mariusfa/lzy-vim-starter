c1310bc838dd97322c794aea80327fc6ac7e863b
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-15 20:48:11 +0200

    Add catpuccin theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,18 @@
+-- return {
+--   { "projekt0n/github-nvim-theme" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "github_light",
+--     },
+--   },
+-- }
 return {
-  { "projekt0n/github-nvim-theme" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_light",
+      colorscheme = "catppuccin-latte",
     },
   },
 }
